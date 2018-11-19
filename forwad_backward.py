4ddbb8d00e447ba4887a862d564c525b587dd52e
naka_t <naka.t06+atlassian@gmail.com>
2018-11-19 19:15:14 +0900

    時間計測修正

--- forwad_backward.py
+++ forwad_backward.py
@@ -6,19 +6,18 @@ from multiprocessing import Pool
 def _foward_backward( args ):
     gpsegm, d = args
 
-    start = time.clock()
+    start = time.time()
     print( "forward...", end="")
     a = gpsegm.forward_filtering( d )
 
     print( "backward...", end="" )
     segm, segm_class = gpsegm.backward_sampling( a, d )
-    print( time.clock()-start, "sec" )
+    print( time.time()-start, "sec" )
 
     return segm, segm_class
 
 
 def forward_backward_mp(gpsegm, data, num_threads):
-    print(num_threads, len(data) )
     pool = Pool( num_threads )
     args = [ (gpsegm, d) for d in data ]
     ret = pool.map(_foward_backward, args )
